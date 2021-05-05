517ba123640067952671e889cb6e7f34207dcf36
samrg472 <samrg472@gmail.com>
2021-05-04 17:43:16 -0700

    Save & restore volumes for headphones and speakers

--- SBC-Control/CmdBottomBarView.cs
+++ SBC-Control/CmdBottomBarView.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Reflection;
 using System.Windows.Threading;
 
@@ -25,21 +27,140 @@ namespace SBC_Control
                 Trace.Assert(method != null, nameof(method) + " != null");
                 _dispatcher.Invoke(() =>
                 {
-                    method.Invoke(_bottomBar, new object[1] { value });
+                    method.Invoke(_bottomBar, new object[] { value });
                 });
             }
         }
+
+        private float MasterVolume
+        {
+            get
+            {
+                var method = _bottomBar.GetType().GetMethod("get_Volume");
+
+                Trace.Assert(method != null, nameof(method) + " != null");
+                return (float) method.Invoke(_bottomBar, new object[0]);
+            }
+            set
+            {
+                var method = _bottomBar.GetType().GetMethod("set_Volume", new[] { typeof(float) });
+
+                Trace.Assert(method != null, nameof(method) + " != null");
+                _dispatcher.Invoke(() =>
+                {
+                    method.Invoke(_bottomBar, new object[] { value });
+                });
+            }
+        }
+
+        private bool MasterMute
+        {
+            get
+            {
+                var method = _bottomBar.GetType().GetMethod("get_Mute");
+
+                Trace.Assert(method != null, nameof(method) + " != null");
+                return (bool) method.Invoke(_bottomBar, new object[0]);
+            }
+            set
+            {
+                var method = _bottomBar.GetType().GetMethod("set_Mute", new[] { typeof(bool) });
+
+                Trace.Assert(method != null, nameof(method) + " != null");
+                _dispatcher.Invoke(() =>
+                {
+                    method.Invoke(_bottomBar, new object[] { value });
+                });
+            }
+        }
+
+        /// <summary>
+        /// Used to determine whether the property change handler is using the same value or raising a different value.
+        /// </summary>
+        private bool _prevIsHeadphoneVal;
 
         public CmdBottomBarView(object bottomBar)
         {
             Trace.Assert(bottomBar != null, nameof(bottomBar) + " != null");
             _bottomBar = bottomBar;
+            _prevIsHeadphoneVal = IsHeadphone;
 
             var type = bottomBar.GetType();
+
             _dispatcher = (Dispatcher) type
                 .GetField("mainThreadDispatcher", BindingFlags.NonPublic | BindingFlags.Instance)?
                 .GetValue(bottomBar);
             Trace.Assert(_dispatcher != null, nameof(_dispatcher) + " != null");
+
+            {
+                // Perform configuration updates
+                var config = GlobalConfig.Config;
+                if (GlobalConfig.ConfigIsNew)
+                {
+                    config.HeadphoneMasterVolume = MasterVolume;
+                    config.HeadphoneMasterMute = MasterMute;
+                    config.SrsMasterVolume = MasterVolume;
+                    config.SrsMasterMute = MasterMute;
+                }
+
+                if (IsHeadphone)
+                {
+                    config.HeadphoneMasterVolume = MasterVolume;
+                    config.HeadphoneMasterMute = MasterMute;
+                }
+                else
+                {
+                    config.SrsMasterVolume = MasterVolume;
+                    config.SrsMasterMute = MasterMute;
+                }
+
+
+                config.Save();
+            }
+
+            {
+                // Install our property change handler
+                var propChangedEvt = type.GetEvent("PropertyChanged", BindingFlags.Public | BindingFlags.Instance);
+                Trace.Assert(propChangedEvt != null, nameof(propChangedEvt) + " != null");
+                var propChangedDel = propChangedEvt.EventHandlerType;
+                var handler =
+                    typeof(CmdBottomBarView).GetMethod("OnPropChanged", BindingFlags.NonPublic | BindingFlags.Instance);
+                var d = Delegate.CreateDelegate(propChangedDel, this, handler ?? throw new InvalidOperationException());
+
+                var propChangedAddMethod = propChangedEvt.GetAddMethod();
+                propChangedAddMethod.Invoke(bottomBar, new object[] {d});
+            }
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        // ReSharper disable once UnusedParameter.Local
+        private void OnPropChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != "IsHeadphone") return;
+            if (IsHeadphone == _prevIsHeadphoneVal) return;
+            _prevIsHeadphoneVal = IsHeadphone;
+
+            var config = GlobalConfig.Config;
+            if (IsHeadphone)
+            {
+                // Save to SRS config
+                config.SrsMasterVolume = MasterVolume;
+                config.SrsMasterMute = MasterMute;
+
+                MasterVolume = config.HeadphoneMasterVolume;
+                MasterMute = config.HeadphoneMasterMute;
+            }
+            else
+            {
+                // Save to headphone config
+                config.HeadphoneMasterVolume = MasterVolume;
+                config.HeadphoneMasterMute = MasterMute;
+
+                MasterVolume = config.SrsMasterVolume;
+                MasterMute = config.SrsMasterMute;
+            }
+
+            config.Save();
         }
     }
 }
---
+++ SBC-Control/GlobalConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SBC_Control
+{
+    public class GlobalConfig
+    {
+        private static GlobalConfig _config;
+
+        public static GlobalConfig Config
+        {
+            get
+            {
+                if (_config != null)
+                    return _config;
+
+                var configPath = GetConfigPath();
+                var serializer = new XmlSerializer(typeof(GlobalConfig));
+                using var stream = new FileStream(configPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+                if (stream.Length == 0)
+                {
+                    var config = new GlobalConfig();
+                    serializer.Serialize(stream, config);
+                    ConfigIsNew = true;
+                    return config;
+                }
+
+                _config = (GlobalConfig) serializer.Deserialize(stream);
+                return _config;
+            }
+        }
+
+        public static bool ConfigIsNew { get; private set; }
+
+        public float HeadphoneMasterVolume;
+        public bool HeadphoneMasterMute;
+
+        public float SrsMasterVolume;
+        public bool SrsMasterMute;
+
+        public void Save()
+        {
+            var configPath = GetConfigPath();
+            var serializer = new XmlSerializer(typeof(GlobalConfig));
+            using var stream = new FileStream(configPath, FileMode.Open, FileAccess.Write);
+            serializer.Serialize(stream, this);
+        }
+
+        private static string GetConfigPath()
+        {
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var configDir = Path.Combine(appData, "SBC_Control");
+            if (!Directory.Exists(configDir))
+            {
+                Directory.CreateDirectory(configDir);
+            }
+
+            return Path.Combine(configDir, "config.xml");
+        }
+    }
+}
